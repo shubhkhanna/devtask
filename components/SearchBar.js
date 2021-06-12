d0532383ed36f01a8bf0f302e0256eb3391eca3e
shubhkhanna <dit.shubhkhanna@gmail.com>
2021-06-12 23:05:15 +0530

    feat: add search bar component

---
+++ components/SearchBar.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+import {View, TextInput} from 'react-native';
+import {COLORS, FONTS, SIZES} from '../constants';
+
+const SearchBar = () => {
+  const [value, setValue] = useState();
+
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        height: 50,
+        alignItems: 'center',
+        marginVertical: SIZES.radius,
+        marginHorizontal: SIZES.padding,
+        paddingHorizontal: SIZES.radius,
+        borderRadius: SIZES.radius,
+        backgroundColor: COLORS.white,
+        borderWidth: 2,
+        borderColor: COLORS.grey,
+      }}>
+      <TextInput
+        value={value}
+        onChangeText={e => setValue(e)}
+        underlineColorAndroid="transparent"
+        style={{
+          flex: 1,
+          color: COLORS.black,
+          marginLeft: SIZES.base,
+          ...FONTS.body4,
+        }}
+        placeholderTextColor={COLORS.grey}
+        placeholder="Search"
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
