d334055d4f9f313b55e81fec455b392aab1f6225
shubhkhanna <dit.shubhkhanna@gmail.com>
2021-06-14 00:12:05 +0530

    feat: add MainLayout -> new prop

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -2,9 +2,10 @@ import React from 'react';
 import {SafeAreaView} from 'react-native';
 import {COLORS} from '../constants';
 
-const MainLayout = ({children}) => {
+const MainLayout = ({children, containerStyle}) => {
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.smokeWhite}}>
+    <SafeAreaView
+      style={{flex: 1, backgroundColor: COLORS.smokeWhite, ...containerStyle}}>
       {children}
     </SafeAreaView>
   );
