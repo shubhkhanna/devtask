73319e8c2cea7ad3908d1cdf27008db305bf8915
shubhkhanna <dit.shubhkhanna@gmail.com>
2021-06-14 00:14:27 +0530

    feat: post component in home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import {View, Text} from 'react-native';
-import {COLORS} from '../constants';
+import React, {useState, useEffect} from 'react';
+import {FlatList} from 'react-native';
+import axios from 'axios';
+import {MainLayout, Post} from '../components';
+import {SIZES} from '../constants';
 
 const Home = () => {
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    const fetchFeeds = async () => {
+      const {data} = await axios.get('https://picsum.photos/v2/list?limit=10');
+      setProperties(data);
+    };
+
+    fetchFeeds();
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: COLORS.smokeWhite,
-      }}>
-      <Text>Home Screen</Text>
-    </View>
+    <MainLayout containerStyle={{flexDirection: 'column'}}>
+      {/* Feeds list */}
+      <FlatList
+        data={properties}
+        showsVerticalScrollIndicator={false}
+        snapToAlignment="start"
+        decelerationRate="normal"
+        snapToInterval={SIZES.height - 70}
+        keyExtractor={item => `${item.id}`}
+        renderItem={({item}) => <Post post={item} />}
+      />
+    </MainLayout>
   );
 };
 
